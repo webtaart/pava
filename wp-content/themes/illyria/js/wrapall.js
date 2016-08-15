878124e1139d75fa8902c3ed4fc1d0041fc0588e
tngvndrm <tanguy.van.durme@gmail.com>
2016-08-15 10:01:40 +0200

    theme remove
    removed old theme references

--- wp-content/themes/illyria/js/wrapall.js
+++
@@ -1,7 +0,0 @@
-
-jQuery( document ).ready(function(){
-		
-	jQuery('header> .wide-header, header >.wrapper').wrapAll("<div class='wrap-elements'></div>");
-	
-	// moves 2 predefined divs in a new one, 'wrap-elements'
-});
